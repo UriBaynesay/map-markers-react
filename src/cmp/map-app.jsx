023e037a78cf576277254db807ed78498d1d18e9
Uri Baynesay <uri552@gmail.com>
2022-09-06 11:35:55 +0300

    map done

--- src/cmp/map-app.jsx
+++ src/cmp/map-app.jsx
@@ -7,11 +7,14 @@ export const MapApp=()=>{
     const [markers,setMarkers]=useState([])
 
     const onAddMarker=(marker)=>{
-        setMarkers([...markers,marker])
+        setMarkers([
+          ...markers,
+          { lat: marker.latLng.lat(), lng: marker.latLng.lng()},
+        ])
     }
 
     return <section className="map-app-container">
         <MarkersList markers={markers}/>
-        <Map onAddMarker={onAddMarker}/>
+        <Map onAddMarker={onAddMarker} markers={markers}/>
     </section>
 }
