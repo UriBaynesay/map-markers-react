12143715ce0ffc51ffce74daa9f8bfe9ac8295e4
Uri Baynesay <uri552@gmail.com>
2022-10-13 14:16:15 +0300

    added logo

--- src/cmp/app-header.jsx
+++ src/cmp/app-header.jsx
@@ -1,6 +1,7 @@
 export const AppHeader = () => {
   return (
     <header className="app-header full">
+      <img src={require("../assets/Images/pin-svgrepo-com.svg").default} alt="" />
       <h1>Map Markers</h1>
     </header>
   )
