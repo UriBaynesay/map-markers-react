6fbb8405f5278837e53c539047258b6ea3e188d1
Uri Baynesay <uri552@gmail.com>
2022-10-20 12:50:42 +0300

    added responsivness for moblie

--- src/cmp/map.jsx
+++ src/cmp/map.jsx
@@ -1,10 +1,6 @@
 import React from "react"
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api"
 
-const containerStyle = {
-  width: "100%",
-  height: "70vh",
-}
 
 const center = {
   lat: -3.745,
@@ -19,7 +15,6 @@ function _Map({ onAddMarker, markers, onRemoveMarker }) {
 
   return isLoaded ? (
     <GoogleMap
-      mapContainerStyle={containerStyle}
       center={center}
       zoom={15}
       onRightClick={onAddMarker}
